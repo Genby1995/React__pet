f64eaced68494f99134ca57e59293c634cbe209d
Genby1995 <n.putintzev@gmail.com>
2022-11-25 13:39:21 +0300

    First draw

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const SideMenu = () => {
+    return (
+        <nav className='side-menu'>
+            <div> <a>Hot</a> </div>
+            <div> <a>Best</a> </div>
+            <div> <a>Fresh</a> </div>
+            <div> <a>My profile</a> </div>
+        </nav>
+    )
+}
+
+export default SideMenu;
